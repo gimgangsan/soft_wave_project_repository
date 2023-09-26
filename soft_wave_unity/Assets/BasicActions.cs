4742c05f78d6ab42779044fbccad2481791bc45c
gimgangsan <gimgansan@gmail.com>
2023-09-27 01:53:38 +0900

    more action when player lost hp and player lost amour

--- soft_wave_unity/Assets/BasicActions.cs
+++ soft_wave_unity/Assets/BasicActions.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class BasicActions : MonoBehaviour
 {
+
     public Action<int> onDamaged;
+    public Action<int> onReducedAmour;
+    public Action<int> onReducedHp;
     public Action onDead;
 
     public int HP = 500;
+    public int Amour = 0;
+
+    public int Defense = 0;
+    public int DamageRatio = 1;
 
     private void Awake()
     {
@@ -37,16 +44,35 @@ public class BasicActions : MonoBehaviour
         
     }
 
-    void GetDamage(int amount)
+    void GetDamage(int power)
     {
-        HP -= amount;
-        if (onDamaged != null) onDamaged(amount);
+        int Damage = CalculateDamage(power);
+        if (Damage <= Amour)
+        {
+            Amour -= Damage;
+            onReducedAmour?.Invoke(Damage);
+        }
+        else
+        {
+            int ActualHarm = Damage - Amour;
+            onReducedAmour?.Invoke(Amour);
+            Amour = 0;
+            this.HP -= ActualHarm;
+            onReducedHp?.Invoke(ActualHarm);
+        }
+        onDamaged?.Invoke(Damage);
+
         Debug.Log(HP);
 
         if (HP <= 0)
         {
-            if(onDead != null) onDead();
+            onDead?.Invoke();
             Debug.Log("player died");
         }
+    }
+
+    public int CalculateDamage(int power)
+    {
+        return (power - Defense) * DamageRatio;
     }
 }

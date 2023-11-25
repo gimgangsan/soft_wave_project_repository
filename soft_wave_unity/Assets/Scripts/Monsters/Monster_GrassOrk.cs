6e1c2ed336abe72d6a2b14087976d3feb8f13947
kevin8337 <kevin7372@naver.com>
2023-11-25 16:35:29 +0900

    몬스터 GrassOrk 추가
    몬스터가 플레이어의 길을 막지 않도록 isTrigger 활성화

---
+++ soft_wave_unity/Assets/Scripts/Monsters/Monster_GrassOrk.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_GrassOrk : MonsterBehavior
+{
+    public Monster_GrassOrk() : base()
+    {
+        health = 3f;
+        speed = 2f;
+        contactDistance = 1f;
+        attackCoolRate = 1f;
+        attackRange = 2f;
+        attackDamage = 10;
+    }
+}

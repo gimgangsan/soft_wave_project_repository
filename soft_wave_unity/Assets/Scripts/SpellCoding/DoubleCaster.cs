1e8883352e63557b0105b5434330da1636fa47e4
gimgangsan <75228703+gimgangsan@users.noreply.github.com>
2023-11-28 23:53:08 +0900

    spell caster 프리펩 추가

--- soft_wave_unity/Assets/Scripts/SpellCoding/DoubleCaster.cs
+++ soft_wave_unity/Assets/Scripts/SpellCoding/DoubleCaster.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 
 public class DoubleCaster : HeadAndTail
 {
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            AimInfo aimInfo;
-            aimInfo.ShooterPos = new Vector2(0, 0);
-            aimInfo.MousePos = new Vector2(1, 1);
-            OnUse(aimInfo);
-        }
-    }
     public override void ReleaseSpell(AimInfo aimInfo)
     {
         Debug.Log("doubleCaster casted");
